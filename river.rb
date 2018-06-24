7a3678575e00c4347b3dea532b0f6eed8f50de3e
nicklennon <nicklennon.53@gmail.com>
2018-06-24 16:55:59 +0000

    Done.

--- river.rb
+++ river.rb
@@ -1,1 +1,33 @@
 # river.rb
+
+class River
+
+  attr_accessor :length, :countries, :discharge
+  
+  def initialize(name)
+    @name = name
+  end
+
+  def name=(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+
+  def flood
+    @discharge = @discharge *1.3
+  end
+  
+  def dry_up
+    @discharge = @discharge *0.5
+  end
+end
+
+nile = River.new("The Nile")
+mississippi = River.new("The Mississippi")
+amazon = River.new("The Amazon")
+seine = River.new("The Seine")
+yangtze = River.new("The Yangtze")
+euphrates = River.new("The Euphrates")
